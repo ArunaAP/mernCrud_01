a681c776174b02b4c4035d153e54b74ace52414e
ArunaAP <arunapriyankara99.ap@gmail.com>
2022-10-10 14:44:10 +0530

    create mern app

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default class App extends Component {
+constructor(props){
+  super(props);
+
+  this.state={
+    posts:[]
+  };
 }
 
-export default App;
+componentDidMount(){
+  this.retrievePosts();
+}
+
+retrievePosts(){
+  axios.get("http://localhost:8000/post").then(res =>{
+    if(res.data.success){
+      this.setState({
+        posts:res.data.existingPosts
+      });
+
+      console.log(this.state.posts);
+    }
+  });
+}
+
+
+  render() {
+    return (
+      <div className='container'>
+        <h2>All posts</h2>
+        <table className='table'>
+          <thead>
+            <tr>
+              <th scope='column'>No</th>
+              <th scope='column'>Topic</th>
+              <th scope='column'>Description</th>
+              <th scope='column'>Post Category</th>
+              <th scope='column'>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.posts.map((posts,index) =>(
+              <tr>
+                <th scope='row'>{index+1}</th>
+                <td>{posts.topic}</td>
+                <td>{posts.desc}</td>
+                <td>{posts.postCategory}</td>
+                <td>
+                  <a className='btn btn-warning' href='#'>
+                    <i className='fas fa-edit'></i>&nbsp;Edit
+                  </a>
+                </td>
+                
+                <td>
+                  <a className='btn btn-danger' href='#'>
+                    <i className='far fa-trash'></i>&nbsp;Delete
+                  </a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+
+
+        </table>
+      </div>
+    )
+  }
+}
